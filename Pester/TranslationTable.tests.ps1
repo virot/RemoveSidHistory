fd3f343abc4ac536c597a1b7bc90959a1d4b9932
Oscar Virot <virot@virot.com>
2016-12-03 18:09:51 +0100

    more tests

--- Pester/TranslationTable.tests.ps1
+++ Pester/TranslationTable.tests.ps1
@@ -19,4 +19,49 @@ Describe "TranslationTable" {
          Clear-TranslationTable
          (Get-TranslationTable).Count | Should Be '0'
     }
+    It "Export TranslationTable" {
+         Add-TranslationTableEntry -SourceSID 'BU' -DestinationSID 'DU'
+         Add-TranslationTableEntry -SourceSID 'S-1-0-0' -DestinationSID 'DA'
+         Add-TranslationTableEntry -SourceSID 'S-1-5-21-1-2-3-4' -DestinationSID 'AU'
+         $tempfile = [system.io.path]::GetTempFileName()
+         Export-TranslationTable -Path $tempfile
+         (Get-FileHash $tempfile -Algorithm SHA1).Hash | Should Be '1827D705FFFBD8D3CCFF20D514A38FAE6F8BA48F'
+         Remove-Item $tempfile
+    }
+    It "Import TranslationTable" {
+         $tempfile = [system.io.path]::GetTempFileName()
+         @'
+<Objs Version="1.1.0.1" xmlns="http://schemas.microsoft.com/powershell/2004/04">
+  <Obj RefId="0">
+    <TN RefId="0">
+      <T>System.Collections.Hashtable</T>
+      <T>System.Object</T>
+    </TN>
+    <DCT>
+      <En>
+        <S N="Key">S-1-0-0</S>
+        <S N="Value">DA</S>
+      </En>
+      <En>
+        <S N="Key">S-1-5-21-1-2-3-4</S>
+        <S N="Value">AU</S>
+      </En>
+      <En>
+        <S N="Key">BU</S>
+        <S N="Value">DU</S>
+      </En>
+    </DCT>
+  </Obj>
+</Objs>
+'@ | Set-Content $tempfile
+         Import-TranslationTable -Path $tempfile
+         (Get-TranslationTable).Count | Should Be '3'
+         (Get-TranslationTable)['BU'] | Should Be 'DU'
+         (Get-TranslationTable)['DU'] | Should Be $Null
+         (Get-TranslationTable).ContainsKey('BU') | Should Be $True
+         (Get-TranslationTable).ContainsKey('DU') | Should Be $False
+         Remove-Item $tempfile
+    }
+
+
 }
