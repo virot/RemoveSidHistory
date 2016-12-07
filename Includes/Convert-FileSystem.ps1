d257a846a9f023d9973109f0f797b42f5bf71787
Oscar Virot <virot@virot.com>
2016-12-07 20:34:18 +0100

    Lots of changes to make recurse work

--- Includes/Convert-FileSystem.ps1
+++ Includes/Convert-FileSystem.ps1
@@ -28,15 +28,13 @@ Function Convert-FileSystem
   }
   Process
   {
-    $aclparam = @{}
-    if ($PSBoundParameters.ContainsKey('SwitchOwner')) {$aclparam.add('ChangeOwner',$ChangeOwner)}
-    if ($PSBoundParameters.ContainsKey('ForceOwner')) {$aclparam.add('ForceOwner',$ForceOwner)}
-    if ($PSBoundParameters.ContainsKey('SwitchGroup')) {$aclparam.add('ChangeGroup',$ChangeGroup)}
-    if ($PSBoundParameters.ContainsKey('ForceGroup')) {$aclparam.add('ForceGroup',$ForceGroup)}
+    $SendAlongParam = @{} + $PSBoundParameters
+    $SendAlongParam.Remove('Recurse')|Out-Null
+    $SendAlongParam.Remove('Path')|Out-Null
     $currentacl = Get-ACL -Literalpath $Path
 $currentacl.sddl
 #why does Convert-ACL change the $currentacl variable
-    $newacl = Convert-ACL -ACL $currentAcl @aclparam
+    $newacl = Convert-ACL -ACL $currentAcl @SendAlongParam
 $currentacl.sddl
     $currentacl = Get-ACL -Literalpath $Path
 $newacl.sddl
@@ -44,12 +42,15 @@ $newacl.sddl
     if ($currentacl.sddl -ne $newacl.sddl -and $pscmdlet.ShouldProcess("$path", "Set New ACL"))
     {
       Write-Host -NoNewline "Setting ACL on $path"
-      $CorrectACL = New-Object System.Security.AccessControl.Directorysecurity 
-      $CorrectACL.SetSecurityDescriptorSddlForm($newSDDL)
-      Set-ACL -LiteralPath $Path  -AclObject $CorrectACL
-      Write-Host '[DONE]'
+      Set-ACL -LiteralPath $Path  -AclObject $newacl
+      Write-Host ' [DONE]'
     }
 #Recursive parts
-    Get-ChildItem -Directory $Path -PipelineVariable ChildDirs | %  {& $($MyInvocation.MyCommand.Name) -Path $ChildDirs.FullName -Recurse:$Recurse -SaveOld:$SaveOld}
+    if ($PSBoundParameters.ContainsKey('Recurse'))
+    {
+      $SendAlongParam = @{} + $PSBoundParameters
+      $SendAlongParam.Remove('Path')|Out-Null
+      Get-ChildItem -Directory $Path -PipelineVariable ChildDirs | %  {& $($MyInvocation.MyCommand.Name) -Path $ChildDirs.FullName @SendAlongParam}
+    }
   }
 }
