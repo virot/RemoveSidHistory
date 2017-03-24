77385d92ee9f59f4b0a1e19a266fbb02e1693818
Oscar Virot <virot@virot.com>
2017-03-24 18:50:00 +0100

    FIX: when switching from 1 sidhistory to multiple once change was missed.. *PESTER NEEDED*

--- Includes/Add-TranslationTableFromAD.ps1
+++ Includes/Add-TranslationTableFromAD.ps1
@@ -7,7 +7,7 @@ Function Add-TranslationTableFromAD
   $searcher.FindAll() | % {
     ForEach ($SidHistory in ($_.Properties['sidhistory']))
     {
-      Add-TranslationTableEntry -SourceSID (new-object System.Security.Principal.SecurityIdentifier $SidHistory,0).toString() -DestionationSID (new-object System.Security.Principal.SecurityIdentifier ($_.Properties['objectsid'][0],0)).toString()
+      Add-TranslationTableEntry -SourceSID (new-object System.Security.Principal.SecurityIdentifier $SidHistory,0).toString() -DestionationSID (new-object System.Security.Principal.SecurityIdentifier $SidHistory,0).toString()
     }
   }
 }
